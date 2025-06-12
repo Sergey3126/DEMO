02a89019ddf8f17742251a2d3baa0d243249776a
Sergey3126 <sergeyromanovich2009@gmail.com>
2025-06-12 17:02:56 +0300

    first commit

---
+++ src/main/java/controller/web/controllers/advice/User.java
@@ -0,0 +1,61 @@
+package controller.web.controllers.advice;
+
+
+public class User {
+    private String name;
+    private String surname;
+    private String mail;
+    private String nickname;
+
+    public User(String name, String surname, String mail, String nickname) {
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+        this.nickname = nickname;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "имя='" + name + '\'' +
+                "фамилия='" + surname + '\'' +
+                "почта='" + mail + '\'' +
+                "ник='" + nickname + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/controller/web/controllers/rest/RestAdminPanelController.java
@@ -0,0 +1,88 @@
+package controller.web.controllers.rest;
+
+
+import controller.web.controllers.advice.User;
+import controller.web.controllers.advice.CreationUserList;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+
+@RestController
+@RequestMapping("/api/admin/")
+public class RestAdminPanelController {
+    Map<String, User> map = new HashMap<>();
+    CreationUserList creationUserList = new CreationUserList();
+    File file = new File("Users.txt");
+    public RestAdminPanelController() {
+
+    }
+
+    @GetMapping(value = {"deleteall", "deleteall/"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public String deleteAll(){
+        file.delete();
+        return "Пользователи удалены";
+    }
+
+
+    @GetMapping(value = {"userinfo", "userinfo/"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public String getInfo(@RequestParam String nick) throws IOException {
+
+        map = creationUserList.GiveUser(nick);
+        if (map.containsKey(nick)) {
+            return String.valueOf(map.get(nick));
+        }
+        return "Такой пользователь не найден";
+    }
+
+    @PostMapping(value = {"createuser", "createuser/"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public String createUser(@RequestBody User user) {
+        String nick = user.getNickname();
+        if (!file.exists()) {
+            creationUserList.SaveUser(user, true);
+            return "Пользователь создан " + user.toString();
+        }
+        map = creationUserList.GiveUser(nick);
+        if (!map.containsKey(nick)) {
+            creationUserList.SaveUser(user, true);
+            return "Пользователь создан " + user.toString();
+        }
+
+        return "Пользователь существует ";
+    }
+
+
+    @DeleteMapping(value = {"/deleteuser/{nick}", "/deleteuser/{nick}/"}, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public String DeleteUser(@PathVariable String nick)  throws IOException {
+
+        Boolean bol = false;
+        if (!file.exists()) {
+            return "Пользователей нет " ;
+        }
+        map = creationUserList.GiveUser(nick);
+        if (map.containsKey(nick)) {
+            map.remove(nick);
+            for (Map.Entry<String, User> entry : map.entrySet()) {
+                User user = entry.getValue();
+                creationUserList.SaveUser(user, bol);
+                bol = true;
+            }
+            return "Пользователь удален";
+        }
+        return "Пользователь не найден";
+    }
+}
+
+
